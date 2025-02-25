7076a2e88bbf7881aabab5797aaaae8e1f59c8fb
Fazal Khan <ggl.acc007@gmail.com>
2025-02-26 02:49:38 +0530

    adding media

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
           
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={bitsOfCode}
+              //imgPath={cardiac}
               isBlog={false}
               title="Secure Cardiac Monitoring"
               description="A real-time ECG & SpO2 monitoring system using Raspberry Pi 4B. Secured with Hyperledger Fabric blockchain & Attribute-Based Encryption, it leverages AWS IoT Greengrass for edge management and Grafana for live monitoring."
@@ -30,7 +30,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={editor}
+              //imgPath={smartBin}
               isBlog={false}
               title="Smart Dustbin"
               description="An AI-powered waste sorter using TensorFlow Lite on Raspberry Pi 4B, achieving 91% accuracy. A servo-driven 360° rotation ensures precise disposal into categorized compartments."
@@ -41,7 +41,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={leaf}
+              //imgPath={smartLib}
               isBlog={false}
               title="SmartLib"
               description="An RFID-based library system with Arduino & Python UI, automating book tracking, issuance, and fines. Features real-time barcode scanning using OpenCV & Pyzbar."
@@ -52,7 +52,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={suicide}
+              //imgPath={appWin}
               isBlog={false}
               title="AppWin"
               description="A Java-based Employee Management App with add, remove, and search features, built using Java Swing & AWT for an intuitive UI."
@@ -63,7 +63,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              //imgPath={emotion}
+              //imgPath={beachView}
               isBlog={false}
               title="Beach View"
               description="The Beach-View Hotel Management System is a web-based platform that allows customers to browse rooms, check availability, view pricing, and make reservations seamlessly. Built with Django, HTML, CSS, and MySQL, it streamlines hotel operations by providing booking management, room services, and an admin panel for efficient hotel administration."
