87b64db3654372324a82b0a124bf444da9f5acde
Fazal Khan <ggl.acc007@gmail.com>
2025-02-26 02:09:52 +0530

    fixing url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/itsfazalkhan/portfolio"
+                href="https://github.com/itsfazalkhan/itsfazalkhan.github.io/"
                 target="_blank"
                 className="fork-btn-inner"
               >
